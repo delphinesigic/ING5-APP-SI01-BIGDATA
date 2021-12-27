c458ae5d446679333706dd11eafef27d65c82959
Mary <marymaatouk1@hotmail.com>
2021-12-27 10:11:17 +0100

    add new version java

---
+++ code/hbase/src/main/java/hbase/Apps.java
@@ -0,0 +1,14 @@
+package hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+public class Apps {
+	
+	static Configuration conf = HBaseConfiguration.create();
+	
+	public static void main(String[] args) {
+		conf.set("", "");
+	}
+
+}
